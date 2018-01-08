b8fca12e34cb40776404069322e0634d0c8df589
Jarno Rostedt <jarno.a.rostedt@student.samk.fi>
2018-01-08 18:26:56 +0200

    Updated files. Working!

--- PO2Sovellus/Startup.cs
+++ PO2Sovellus/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -12,14 +13,28 @@ namespace PO2Sovellus
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+        // Constructor for Startup
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder();
+            builder.SetBasePath(env.ContentRootPath);
+            builder.AddJsonFile("appsettings.json");
+            builder.AddEnvironmentVariables();
+            builder.Build();
+            Configuration = builder.Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(Configuration);
+            services.AddSingleton<ITervehtija, Tervehtija>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, ITervehtija tervehtijä)
         {
             loggerFactory.AddConsole();
 
@@ -30,7 +45,8 @@ namespace PO2Sovellus
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                var message = tervehtijä.GetTervehdys();
+                await context.Response.WriteAsync(message);
             });
         }
     }
---
+++ PO2Sovellus/Tervehtija.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PO2Sovellus
+{
+    public class Tervehtija : ITervehtija
+    {
+        private string _tervehdys;
+
+        public Tervehtija(IConfiguration configuration)
+        {
+            _tervehdys = configuration["Tervehdys"];
+        }
+        public string GetTervehdys()
+        {
+            return _tervehdys;
+        }
+    }
+}
